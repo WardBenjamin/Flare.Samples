e07a3622f7379d4f22b74c7c0895e15c42e9c016
Benjamin Ward <ward.programm3r@gmail.com>
2016-01-28 13:10:35 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Flare;
+
+namespace FlareDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game("Game1", 800, 600);
+            game.IsMouseVisible = true;
+            Console.WriteLine(game.Window.Icon);
+            game.Run();
+        }
+    }
+}
